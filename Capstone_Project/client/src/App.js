37a4f9a10cebe0de24db6dfed4d7623b33e54bab
Connor Cowan <connorcccc@gmail.com>
2022-09-27 12:36:56 -0400

    review table chosen by businessId

--- Capstone_Project/client/src/App.js
+++ Capstone_Project/client/src/App.js
@@ -1,10 +1,11 @@
 import Businesses from "./Components/Businesses";
-
+import Reviews from "./Components/Reviews"; 
 
 function App() {
   return (
     <div className="App">
       <Businesses/> 
+      <Reviews businessId={2} />
     </div>
   );
 }
---
+++ Capstone_Project/client/src/Components/Reviews.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+function Reviews({businessId}){
+    const [reviews,setReviews] = useState([]); 
+    const init = {
+        method:'GET'
+    };
+    useEffect(() => {
+        fetch(`http://localhost:8080/api/review/${businessId}`,init)
+        .then(resp => {
+          if (resp.status === 200) {
+            return resp.json();
+          }
+          return Promise.reject('Something terrible has gone wrong.  Oh god the humanity!!!');
+        })
+        .then(data => {
+          setReviews(data);
+        })
+        // .catch(err => history.push('/error', {errorMessage: err}));
+      },[]);
+
+
+      return (
+        <>
+        <h2>Business Reviews</h2>
+        <table className="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">reviewId</th>
+                    <th scope="col">content</th>
+                    <th scope="col">timeDate</th>
+                    <th scope="col">rating</th>
+                    <th scope="col">personId</th>
+                    <th scope="col">businessId</th>
+                </tr>
+                </thead>
+                <tbody>
+                    {reviews.map((review,index)=>(
+                        <tr key={index}>
+                            <td>{review.reviewId}</td>
+                            <td>{review.content}</td>
+                            <td>{review.timeDate}</td>
+                            <td>{review.rating}</td>
+                            <td>{review.personId}</td>
+                            <td>{review.businessId}</td>
+                        </tr>
+                    ))}
+                </tbody>
+        
+            
+        </table>
+        </>
+      )
+
+
+
+
+}
+export default Reviews; 
