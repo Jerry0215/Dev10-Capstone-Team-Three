25946b93b32d3c188eb298f14d312c59b3d5f98c
Connor Cowan <connorcccc@gmail.com>
2022-09-28 12:18:18 -0400

    map component

--- Capstone_Project/client/src/App.js
+++ Capstone_Project/client/src/App.js
@@ -1,9 +1,10 @@
 
-import Businesses from "./components/Businesses";
-import Reviews from "./components/Reviews"; 
-import Locations from "./components/Locations";
-import Events from "./components/Events";
-import Home from "./components/Home";
+import Businesses from "./Components/Businesses";
+import Reviews from "./Components/Reviews"; 
+import Locations from "./Components/Locations";
+import Events from "./Components/Events";
+import Home from "./Components/Home";
+import Map from "./Components/Map";
 import { BrowserRouter as Router, Link, Redirect, Route, Switch } from 'react-router-dom';
 import { useState } from 'react';
 
@@ -24,6 +25,9 @@ function App() {
         </Route>
         <Route exact path="/event">
           <Events/>
+        </Route>
+        <Route exact path="/map">
+          <Map address="15 Capri Court" state="New York"></Map>
         </Route>
         </div>
       </Router>
