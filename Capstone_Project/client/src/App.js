bdb9c783ed73a48693b869b171937e5b9831f9df
Connor Cowan <connorcccc@gmail.com>
2022-09-28 14:57:22 -0400

    added business page

--- Capstone_Project/client/src/App.js
+++ Capstone_Project/client/src/App.js
@@ -5,6 +5,7 @@ import Locations from "./Components/Locations";
 import Events from "./Components/Events";
 import Home from "./Components/Home";
 import Map from "./Components/Map";
+import Persons from "./Components/Persons";
 
 import { BrowserRouter as Router, Link, Redirect, Route, Switch } from 'react-router-dom';
 import { useState } from 'react';
