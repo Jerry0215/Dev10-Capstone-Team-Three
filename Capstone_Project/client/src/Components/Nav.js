b4f68a47777785c0ad44cc8a818c9c65ff283898
MF <maryamfarooqui1@gmail.com>
2022-10-01 18:17:47 -0400

    added profile page and link

--- Capstone_Project/client/src/Components/Nav.js
+++ Capstone_Project/client/src/Components/Nav.js
@@ -2,46 +2,59 @@ import { Link } from "react-router-dom";
 import { useContext } from 'react';
 import UserContext from "../UserContext";
 
-function Nav () {
+function Nav() {
   const authManager = useContext(UserContext);
-    return (
+  return (
 
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Nyelp!</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">Nyelp!</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-  <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
 
-    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
 
-      <li className="nav-item active">
-        <Link className="nav-link" to="/">Home<span className="sr-only">(current)</span></Link>
-      </li>
+          <li className="nav-item active">
+            <Link className="nav-link" to="/">Home<span className="sr-only">(current)</span></Link>
+          </li>
+       
+          {authManager.user ?
+              (<>
+                <li className="nav-item active">
+                  <Link to="/profile" className='nav-link'>Profile</Link>
+                </li>
+              </>) : null
+            }
 
-      <li className="nav-item active">
-        <Link className="nav-link" to="/searchpeople">Search People</Link>
-      </li>
+          <li className="nav-item active">
+            <Link className="nav-link" to="/searchpeople">Search People</Link>
+          </li>
 
-      <li className="nav-item active">
-        <Link className="nav-link" to="/searchbusinesses">Search Businesses</Link>
-      </li>
+          <li className="nav-item active">
+            <Link className="nav-link" to="/searchbusinesses">Search Businesses</Link>
+          </li>
 
-      {!authManager.user ? (<>
+        </ul>
+
+        <ul className="navbar-nav mt-2 mt-lg-0">
+        <div className="right-aligned-links">
+            {!authManager.user ? (<>
               <li className="nav-item active">
                 <Link to="/login" className='nav-link'>Sign In</Link>
-              </li> 
-            </>): 
-            <button type="button" className="btn btn-secondary" onClick={authManager.logout}>Sign Out</button>}
+              </li>
+            </>) :
+              <button type="button" className="btn btn-secondary" onClick={authManager.logout}>Sign Out</button>}
+          </div>
 
-    </ul>
+        </ul>
 
+        
+      </div>
+    </nav>
 
-  </div>
-</nav>
-
-    )
+  )
 
 }
 
