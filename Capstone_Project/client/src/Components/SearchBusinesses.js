71e0cd9fbf1aa01aebea0208fe21fb8d885e826a
MF <maryamfarooqui1@gmail.com>
2022-09-30 21:19:44 -0400

    search businesses

--- Capstone_Project/client/src/Components/SearchBusinesses.js
+++ Capstone_Project/client/src/Components/SearchBusinesses.js
@@ -1,45 +1,62 @@
-import {useHistory} from 'react-router-dom'; 
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+
 import Business from './Business';
 
-function SearchBusinesses(){
-    const [businesses, setBusinesses] = useState([])
-    
-    useEffect(() => {
-        fetch('http://localhost:8080/api/business')
-        .then(resp => {
-          if (resp.status === 200) {
-            return resp.json();
-          }
-          return Promise.reject('Something terrible has gone wrong.  Oh god the humanity!!!');
-        })
-        .then(data => {
-          setBusinesses(data);
-        })
-        //.catch(err => history.push('/error', {errorMessage: err}));
-      },[])
 
-      return (
+function SearchBusinesses() {
+
+    const [businesses, setBusinesses] = useState([]);
+
+    const history = useHistory();
+
+    const [prefix, setPrefix] = useState([]);
+
+    const handleChange = (evt) => setPrefix(evt.target.value);
+
+    const handleSubmit = (evt) => {
+
+        evt.preventDefault();
+
+        fetch(`http://localhost:8080/api/business/search/${prefix}`)
+            .then(resp => {
+                console.log(resp.status);
+                if (resp.status === 200) {
+                    return resp.json();
+                }
+                return Promise.reject('Something has gone wrong in fetching the Business data.');
+            })
+            .then(data => {
+                setBusinesses(data);
+            })
+            .catch(console.log);
+        //.catch(err => history.push('/error', {errorMessage: err}));
+
+    }
+
+
+    return (
         <>
-        <h2>Businesses</h2>
-        <table className ="table table-striped">
-            <thead>
-                <tr>
-                    <th scope="col">businessId</th>
-                    <th scope="col">name</th>
-                    <th scope="col">description</th>
-                    <th scope="col">rating</th>
-                    <th scope="col">locationId</th>
-                    <th scope="col">personId</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                {businesses.map(business=> <Business key={business.businessId} business={business}/>)}
-            </tbody>
-        </table>
+            <h2>Search Businesses</h2>
+
+            <form onSubmit={handleSubmit}>
+                <div className="search-wrapper mb-3">
+                    <input className="form-control mr-sm-2" type="text" placeholder="Search" id="search" value={prefix} onChange={handleChange} />
+                    <button className="btn btn-outline-success my-2 my-sm-0 btn btn-light" type="submit">Search</button>
+                </div>
+            </form>
+
+            <table className="table table-striped">
+                <tbody>
+                    {businesses.map(business => <Business key={business.businessId} business={business} />)}
+                </tbody>
+            </table>
+
+
         </>
-      );
+    )
+
 }
 
-export default SearchBusinesses; 
+export default SearchBusinesses;
+
