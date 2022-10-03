1e1b629e0381f5e00eb0e54bb13a974f3046b505
Connor Cowan <connorcccc@gmail.com>
2022-10-03 17:48:41 -0400

    got profile page on navbar working

--- Capstone_Project/client/src/Components/Nav.js
+++ Capstone_Project/client/src/Components/Nav.js
@@ -23,7 +23,7 @@ function Nav() {
           {authManager.user ?
               (<>
                 <li className="nav-item active">
-                  <Link to="/personpage/:personId" className='nav-link'>Profile</Link>
+                  <Link to={"/personpage/" +authManager.user.personId} className='nav-link'>Profile</Link>
                 </li>
               </>) : null
             }
