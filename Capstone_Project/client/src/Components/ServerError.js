096cdc19d1f083ea49c85251f3e24a6be19abdf1
MF <maryamfarooqui1@gmail.com>
2022-10-06 10:17:43 -0400

    server error

--- Capstone_Project/client/src/Components/ServerError.js
+++ Capstone_Project/client/src/Components/ServerError.js
@@ -4,8 +4,8 @@ import { useHistory } from "react-router-dom";
 function ServerError({error}) {
 
     return(<>
-    <h2>Oh noes!</h2>
-    <img src="../../src/pictures/thisisfine.webp" alt="Everything is on fire"/>
+    <h2>Oh noes! Everything is on fire!</h2>
+    {/* <img src="../../src/pictures/thisisfine.webp" alt="Everything is on fire"/> */}
     {error && (<div className="alert alert-danger">{`${error}`}</div>)}
     </>)
 }
